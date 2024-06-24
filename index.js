3e6feaa6852bdd2c881b8995f96163779327e480
Vinay Pandey <vinaypandey46158@gmail.com>
2024-06-24 11:26:15 +0530

    Adding existing file

---
+++ index.js
@@ -0,0 +1,14 @@
+var a = 1;
+
+let myName = false;
+function getMyName() {
+    if(!myName) {
+        console.log("this is not my name ")
+    }
+    else {
+        console.log("yaayyy!!!!!!! This is my name")
+    }
+};
+
+
+getMyName();
